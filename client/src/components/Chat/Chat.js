45c3b992c1da80df61765a55db2df2ddccf95af5
Carlos Gil <crafaelgil@gmail.com>
2021-02-15 02:31:51 +0900

    Fix error in the Chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,48 +15,51 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState('');
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
-    setName(name);
     setRoom(room);
+    setName(name)
 
     socket.emit('join', { name, room }, (error) => {
-      if(error) alert(error);
+      if(error) {
+        alert(error);
+      }
     });
-  },[location.search]);
-
+  }, [ENDPOINT, location.search]);
+  
   useEffect(() => {
     socket.on('message', message => {
-      setMessages([...messages,message]);
+      setMessages(messages => [ ...messages, message ]);
     });
-
-    socket.on('roomData', ({users}) => {
+    
+    socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
-  });
+  }, []);
 
   const sendMessage = (event) => {
-    event.prevenDefault();
+    event.preventDefault();
 
     if(message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
 
-  return(
+  return (
     <div className="outerContainer">
       <div className="container">
-        <InfoBar room={room}/>
-        <Messages messages={messages} name={name} />
-        <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      {/* <TextContainer users={users}/> */}
     </div>
   );
 }
