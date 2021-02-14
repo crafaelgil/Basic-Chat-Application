4b1c6c66eef8a41c4fdda1d1364a3351718d7735
Carlos Gil <crafaelgil@gmail.com>
2021-02-15 02:29:42 +0900

    Fix buggs

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -54,7 +54,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room}/>
-        <Messages messages={messages} name={name}></Messages>
+        <Messages messages={messages} name={name} />
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
       </div>
     </div>
--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import ScrollToBottom from 'react-scroll-to-bottom';
 
 import Message from './Message/Message';
@@ -7,7 +8,7 @@ import './Messages.css';
 
 const Messages = ({ messages, name }) => (
   <ScrollToBottom className="messages">
-    { messages.map((message, i) => <div key={i}><Message message={message} name={name}></Message></div>) }
+    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
   </ScrollToBottom>
 );
 
