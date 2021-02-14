4cfbed976b110302c2a393824cbd0d02a69b40d8
Carlos Gil <crafaelgil@gmail.com>
2021-02-15 01:00:51 +0900

    Implement on sendMessage method on io

--- server/index.js
+++ server/index.js
@@ -27,6 +27,14 @@ io.on('connect', (socket) => {
     callback();
   });
 
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.on(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
     io.on(user.room).emit('message', { user: 'admin', text: `${user.name} has left the room` });
