57fbc4a7662c8fc0f0f06f63453412788d4043fe
Carlos Gil <crafaelgil@gmail.com>
2021-02-15 00:04:19 +0900

    Implement users API

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+const { error } = require('console');
+const { Socket } = require('dgram');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const app = express();
@@ -9,5 +12,13 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(router);
+
+io.on('connect', (socket) => {
+  Socket.on('join', ({ name, room}, callback) => {
+    if(error) return callback(error);
+
+    callback();
+  });
+});
 
 server.listen(process.env.PORT || 5000, () => console.log('Server has started'));
