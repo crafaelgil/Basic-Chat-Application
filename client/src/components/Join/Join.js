9d342c42665a1e1bb666d84822e39bf15ae1f6f6
Carlos Gil <crafaelgil@gmail.com>
2021-02-14 21:51:37 +0900

    Update Join component

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Join.css';
 
 const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
   return(
-    <h1>Join</h1>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1>Join</h1>
+        <div>
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}></input>
+        </div>
+        <div>
+        <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}></input>
+        </div>
+        <link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/Chat?name=${name}&room=${room}`}>
+          <button className="button mt-20" type="submit">Sign In</button>
+        </link>
+      </div>
+    </div>
   );
 }
 
